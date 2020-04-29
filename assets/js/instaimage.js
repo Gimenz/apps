072a4922db30f89b866f56e94e3df7efa8e689a8
gimenz <agaaja187@gmail.com>
2020-04-30 00:37:59 +0700

    apps

---
+++ assets/js/instaimage.js
@@ -0,0 +1,27 @@
+function insta_function(url) {
+    regExp = /((http:\/\/(instagram\.com\/p\/.*\/|www\.instagram\.com\/p\/.*\/))|(https:\/\/(www\.instagram\.com\/p\/.*\/)))/i;
+    match = url.match(regExp);
+    if (match && match[1].length >= 11) {
+        posturl = match[1];
+        imgpreview = posturl + 'media?size=l';
+        imagehq = posturl + 'media?size=l';
+        document.getElementById('imgpreview').src = imgpreview;
+        document.getElementById('imagehq').href = imagehq;
+    } else {
+        alert("Url yang dimasukkan salah, ulangi lagi");
+    }
+}
+$(document).keypress(function (event) {
+    if (event.which == 115 && (event.ctrlKey || event.metaKey) || (event.which == 19)) {
+        event.preventDefault();
+        return false;
+    }
+    return true;
+});
+
+$("#download-buttons").hide();
+$("#imagedloadbtn").click(function () {
+    $("#download-buttons").slideDown(500).fadeIn(250);
+    $("#imagedloadbtn").hide();
+    $(".input-group").hide();
+});
